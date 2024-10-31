e6f6f00feefd7ca7c4c57b44a4e155649c9bd36c
Nana Qofi <nanaqofi@Nanas-MacBook-Pro.local>
2024-10-31 16:35:57 -0400

    deploy: constants fixes

--- src/constants/index.js
+++ src/constants/index.js
@@ -21,7 +21,7 @@ import TRUCK from '../assets/images/Truck.png';
 import VECTOR from '../assets/images/Vector.svg';
 import VISA from '../assets/images/visa.svg';
 import VEGETABLES from '../assets/images/Vegetables.png';
-import ARROW from '../assets/images/arrow.png';
+import ARROW from '../assets/images/Arrow.png';
 import STRAWBERRY from '../assets/images/Strawberry.png';
 import ABRICOT from '../assets/images/Abricot.png';
 import ABOUTHERO from '../assets/images/aboutimage.png';
