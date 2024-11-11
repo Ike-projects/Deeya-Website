c506ae151032a188b1b776b874034fb6f5f126d2
Nana Qofi <nanaqofi@Nanas-MacBook-Pro.local>
2024-11-11 17:25:07 -0500

    node_modules updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,57 +1,62 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-import Home from './pages/Home.jsx'
-import AboutPage from './pages/AboutPage.jsx'
-import AboutUs from './pages/AboutUs.jsx'
-import MarketPlace from './pages/MarketPlace.jsx'
-import ProductDetails from './pages/ProductDetails.jsx'
-import Blog from './pages/Blog.jsx'
-import BlogDetails from './pages/BlogDetails.jsx'
-import EmptyCart from './pages/EmptyCart.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import AboutPage from "./pages/AboutPage.jsx";
+import AboutUs from "./pages/AboutUs.jsx";
+import MarketPlace from "./pages/MarketPlace.jsx";
+import ProductDetails from "./pages/ProductDetails.jsx";
+import Blog from "./pages/Blog.jsx";
+import BlogDetails from "./pages/BlogDetails.jsx";
+import EmptyCart from "./pages/EmptyCart.jsx";
+import Cart from "./pages/Cart.jsx";
 
 const router = createBrowserRouter([
   {
-    path: '/Deeya-Website/',
-    element: <App/>,
+    path: "/Deeya-Website/",
+    element: <App />,
     errorElement: <div>404 Not Found</div>,
     children: [
       {
-        path: '/Deeya-Website/',
-        element: < Home/>
+        path: "Deeya-Website/",
+        element: <Home />,
       },
       {
-        path:'/Deeya-Website/aboutus',
-        element:<AboutPage/>,
+        path: "/Deeya-Website/aboutus",
+        element: <AboutPage />,
       },
       {
-        path:'/Deeya-Website/marketplace',
-        element:<MarketPlace/>,
+        path: "/Deeya-Website/marketplace",
+        element: <MarketPlace />,
       },
       {
-        path:'/Deeya-Website/marketplace/:id',
-        element:<ProductDetails/>,
+        path: "/Deeya-Website/marketplace/:id",
+        element: <ProductDetails />,
       },
       {
-        path:'/Deeya-Website/blog',
-        element:<Blog/>
+        path: "/Deeya-Website/blog",
+        element: <Blog />,
       },
       {
-        path:'/Deeya-Website/blog/:id',
-        element:<BlogDetails/>
+        path: "/Deeya-Website/blog/:id",
+        element: <BlogDetails />,
       },
       {
-        path:'/Deeya-Website/emptycart/',
-        element:< EmptyCart/>
-      }
-    ]
-  }
-])
+        path: "/Deeya-Website/emptycart/",
+        element: <EmptyCart />,
+      },
+      {
+        path: "/Deeya-Website/cart",
+        element: <Cart />,
+      },
+    ],
+  },
+]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
